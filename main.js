bf6371fcd3090adfe5a9fe7439bb3149bc501409
Souttak <facundo.delaporte@gmail.com>
2021-08-23 13:56:24 -0300

    Modified code to use document createElement for task components

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
-const checkbox = `<input type="checkbox" name="finished">`;
-const deleteButton = `<button onclick="removeTask(this)">Del</button>`;
+const taskList = document.getElementById('taskList');
+const taskInput = document.getElementById('taskInput');
 
 function addTask() {
-    const taskList = document.getElementById('taskList');
-    const taskInput = document.getElementById('taskInput');
-
     if (taskInput.value != '' && taskInput.value != null) {
+        // Creating task components.
+        const checkbox = document.createElement('input');
+        checkbox.setAttribute('type', 'checkbox');
+        checkbox.setAttribute('name', 'finished');
+
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = 'Del';
+        deleteButton.setAttribute('onclick', 'removeTask(this)');
+
         const newTask = document.createElement('li');
         newTask.classList.add('task');
-        newTask.innerHTML = `<div class="taskContent"> ${checkbox} <p class="taskDesc"> ${taskInput.value} </p></div> ${deleteButton}`;
+
+        const taskDiv = document.createElement('div');
+        taskDiv.classList.add('taskContent');
+
+        const taskDesc = document.createElement('p');
+        taskDesc.classList.add('taskDesc');
+        taskDesc.innerHTML = taskInput.value;
+
+        // Assembling the task.
+        taskDiv.appendChild(checkbox);
+        taskDiv.appendChild(taskDesc);
+
+        newTask.appendChild(taskDiv);
+        newTask.appendChild(deleteButton);
+
+        // Appending the task to the task list.
         taskList.appendChild(newTask);
     }
 }
